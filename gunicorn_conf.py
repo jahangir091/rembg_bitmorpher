cff45b8b6033f9c336812bf81719007d043e6712
jahangir <jahangir.cse09@gmail.com>
2023-11-30 16:47:32 +0600

    added nginx and gunicorn configurations

---
+++ gunicorn_conf.py
@@ -0,0 +1,14 @@
+# gunicorn_conf.py
+from multiprocessing import cpu_count
+
+bind = "127.0.0.1:8000"
+
+# Worker Options
+# workers = cpu_count() + 1
+workers = 1
+worker_class = 'uvicorn.workers.UvicornWorker'
+
+# Logging Options
+loglevel = 'debug'
+accesslog = '/var/log/rembg/access_log'
+errorlog = '/var/log/rembg/error_log'
--- rembg_install.sh
+++ rembg_install.sh
@@ -5,24 +5,49 @@
 #################################################
 
 
-add-apt-repository ppa:deadsnakes/ppa
-apt update -y
-apt upgrade -y
-apt install python3.10-venv -y
-apt install htop -y
-apt install git -y
-apt install python3-venv libgl1 libglib2.0-0 -y
-git clone https://github.com/jahangir091/rembgtest.git
-cd rembgtest
-python3.10 -m venv env_rembg
-source env_rembg/bin/activate
-pip install rembg
-pip install piexif
-pip install opencv-python
-pip install fastapi
-pip install "uvicorn[standard]" gunicorn
+prepare_installation(){
+  add-apt-repository ppa:deadsnakes/ppa
+  apt update -y
+  apt upgrade -y
+  apt install python3.10-venv -y
+  apt install htop -y
+  apt install git -y
+  apt install nginx -y
+  apt install python3-venv libgl1 libglib2.0-0 -y
+  git clone https://github.com/jahangir091/rembgtest.git
+  cd rembgtest
+  python3.10 -m venv env_rembg
+  source env_rembg/bin/activate
+  pip install -r requirements.txt
+
+  cp rembg.service /etc/systemd/system/
+  service rembg start
+
+  cp rembg.cong /etc/nginx/sites-available/
+  ln -s /etc/nginx/sites-available/rembg.conf /etc/nginx/sites-enabled/
+  service nginx start
+}
+
+start_rembg(){
+  cd rembgtest
+  git fetch
+  git reset --hard origin/main
+  service rembg restart
+}
 
 
+install="install"
+start="start"
 
+if [ "$1" == "$start" ]
+then
+  start_rembg
+elif [ "$1" == "$install" ]
+then
+  prepare_installation
+  start_rembg
+else
+  printf "\n\n expected flags 'install' or 'start' \n\n"
+fi
 #uvicorn main:app --host 0.0.0.0 --port 8001 --reload
-gunicorn main:app --workers "$1" --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:"$2"
+#gunicorn main:app --workers "$1" --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:"$2"
