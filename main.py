578e3c2ce63a5b75e08d8c10bc38c1600682f17a
jahangir <jahangir.cse09@gmail.com>
2024-02-28 13:35:45 +0600

    updated rembg api response with image url

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import piexif.helper
 from datetime import datetime, timezone
 import uvicorn
 import logging.config
+import os, uuid
 
 from fastapi import FastAPI, Body
 import rembg
@@ -30,6 +31,14 @@ app.add_middleware(
 # app.add_middleware(Analytics, api_key="00e0893b-82af-440c-af58-5de94649a57c")
 # Check the link below for the fast api analytics
 # https://pypi.org/project/fastapi-analytics/
+
+
+def get_img_path(directory_name):
+    current_dir = '/tmp'
+    img_directory = current_dir + '/.temp' + directory_name
+    os.makedirs(img_directory, exist_ok=True)
+    img_file_name = uuid.uuid4().hex[:20] + '.jpg'
+    return img_directory + img_file_name
 
 
 def decode_base64_to_image(img_string):
@@ -62,8 +71,8 @@ models = [
     "isnet-anime",
 ]
 
-@app.post("/ai/api/v1/rembg")
-async def rembg_remove(
+@app.post("/ai/api/v1/remove_bg")
+async def remove_image_background(
     input_image: str = Body("", title='rembg input image'),
     model: int = Body(6, title='rembg model, not required, default to 6'),
     return_mask: bool = Body(True, title='return mask, not required, default True'),
@@ -74,9 +83,8 @@ async def rembg_remove(
         return{
             "success": False,
             "message": "Input image not found",
-            "server_hit_time": '',
             "server_process_time": '',
-            "output_image": ''
+            "output_image_url": ''
         }
     utc_time = datetime.now(timezone.utc)
     start_time = time.time()
@@ -94,16 +102,17 @@ async def rembg_remove(
         # alpha_matting_erode_size=alpha_mat_erode_size if alpha_mat_erode_size else 10,
     )
 
-    output_image = encode_pil_to_base64(image).decode("utf-8")
+    # output_image = encode_pil_to_base64(image).decode("utf-8")
+    out_image_path = get_img_path('/rembg/')
+    image[0].save(out_image_path)
 
     print("time taken: {0}".format(time.time()-start_time))
 
     return {
         "success": True,
         "message": "Returned output successfully",
-        "server_hit_time": str(utc_time),
         "server_process_time": time.time()-start_time,
-        "output_image": output_image
+        "output_image": 'media/' + out_image_path.split('/')[-1]
     }
 
 
